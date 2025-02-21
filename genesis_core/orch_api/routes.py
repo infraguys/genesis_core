f878a1db800e22680b2fc2d96030329bd2ccd84c
Anton Kremenetsky <anton.kremenetsky@gmail.com>
2025-02-21 21:53:14 +0300

    Project skeleton with 3 general services (#3)
    Added three services:
    - User api
    - Orch api
    - GService
    
    Signed-off-by: Anton Kremenetsky <anton.kremenetsky@gmail.com>

---
+++ genesis_core/orch_api/routes.py
@@ -0,0 +1,35 @@
+#    Copyright 2025 Genesis Corporation.
+#
+#    All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from restalchemy.api import routes
+
+from genesis_core.orch_api import controllers
+
+
+class HealthRoute(routes.Route):
+    """Handler for /v1/health endpoint"""
+
+    __controller__ = controllers.HealthController
+    __allow_methods__ = [routes.FILTER]
+
+
+class ApiEndpointRoute(routes.Route):
+    """Handler for /v1/ endpoint"""
+
+    __controller__ = controllers.ApiEndpointController
+    __allow_methods__ = [routes.FILTER]
+
+    health = routes.route(HealthRoute)
