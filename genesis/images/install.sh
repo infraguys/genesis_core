be7b5d946ef53217c5d7425fc6a333e2b7a235ee
Anton Kremenetsky <anton.kremenetsky@gmail.com>
2025-02-21 23:27:46 +0300

    GC installation into image, RA adaptation with PG
    Signed-off-by: Anton Kremenetsky <anton.kremenetsky@gmail.com>

--- genesis/images/install.sh
+++ genesis/images/install.sh
@@ -1,5 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ex
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-echo "Hello from genesis-core!"
+set -eu
+set -x
+set -o pipefail
+
+GC_PATH="/opt/genesis_core"
+GC_CFG_DIR=/etc/genesis_core
+VENV_PATH="$GC_PATH/.venv"
+
+GC_PG_USER="genesis_core"
+GC_PG_PASS="genesis_core"
+GC_PG_DB="genesis_core"
+
+SYSTEMD_SERVICE_DIR=/etc/systemd/system/
+
+# Install packages
+sudo apt update
+sudo apt install build-essential python3.12-dev postgresql libev-dev python3.12-venv -y
+
+# Default creds for genesis core services
+sudo -u postgres psql -c "CREATE ROLE $GC_PG_USER WITH LOGIN PASSWORD '$GC_PG_PASS';"
+sudo -u postgres psql -c "CREATE DATABASE $GC_PG_USER OWNER $GC_PG_DB;"
+
+# Install genesis core
+sudo mkdir -p $GC_CFG_DIR
+sudo cp "$GC_PATH/etc/genesis_core/genesis_core.conf" $GC_CFG_DIR/
+sudo cp "$GC_PATH/etc/genesis_core/logging.yaml" $GC_CFG_DIR/
+
+mkdir -p "$VENV_PATH"
+python3 -m venv "$VENV_PATH"
+source "$GC_PATH"/.venv/bin/activate
+pip install pip --upgrade
+pip install -r "$GC_PATH"/requirements.txt
+pip install -e "$GC_PATH"
+deactivate
+
+# Create links to venv
+sudo ln -sf "$VENV_PATH/bin/gc-user-api" "/usr/bin/gc-user-api"
+sudo ln -sf "$VENV_PATH/bin/gc-orch-api" "/usr/bin/gc-orch-api"
+sudo ln -sf "$VENV_PATH/bin/gc-gservice" "/usr/bin/gc-gservice"
+
+# Install Systemd service files
+sudo cp "$GC_PATH/etc/systemd/gc-user-api.service" $SYSTEMD_SERVICE_DIR
+sudo cp "$GC_PATH/etc/systemd/gc-orch-api.service" $SYSTEMD_SERVICE_DIR
+sudo cp "$GC_PATH/etc/systemd/gc-gservice.service" $SYSTEMD_SERVICE_DIR
+
+# Enable genesis core services
+sudo systemctl enable gc-user-api gc-orch-api gc-gservice
