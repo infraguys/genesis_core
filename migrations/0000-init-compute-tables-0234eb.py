7e53b8104047e087f6a72eba8c5517dec530ab0f
Eugene Frolov <eugene@frolov.net.ru>
2025-02-24 12:27:54 +0300

    Add init migration structure (#6)
    * Changed skipsdist to False in tox.ini to enable source distribution
    
    * Add init migration structure
    
    ---------
    
    Co-authored-by: Anton Kremenetsky <anton.kremenetsky@gmail.com>

---
+++ migrations/0000-init-compute-tables-0234eb.py
@@ -0,0 +1,41 @@
+# Copyright 2016 Eugene Frolov <eugene@frolov.net.ru>
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from restalchemy.storage.sql import migrations
+
+
+class MigrationStep(migrations.AbstarctMigrationStep):
+
+    def __init__(self):
+        self._depends = []
+
+    @property
+    def migration_id(self):
+        return "0234eb6a-82b1-49b8-b266-2d0a7b4deac9"
+
+    @property
+    def is_manual(self):
+        return False
+
+    def upgrade(self, session):
+        pass
+
+    def downgrade(self, session):
+        pass
+
+
+migration_step = MigrationStep()
---
+++ migrations/0000-init-iam-tables-9c9153.py
@@ -0,0 +1,41 @@
+# Copyright 2016 Eugene Frolov <eugene@frolov.net.ru>
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from restalchemy.storage.sql import migrations
+
+
+class MigrationStep(migrations.AbstarctMigrationStep):
+
+    def __init__(self):
+        self._depends = []
+
+    @property
+    def migration_id(self):
+        return "9c9153e3-e187-4420-bf4d-d0e4dbf489d3"
+
+    @property
+    def is_manual(self):
+        return False
+
+    def upgrade(self, session):
+        pass
+
+    def downgrade(self, session):
+        pass
+
+
+migration_step = MigrationStep()
