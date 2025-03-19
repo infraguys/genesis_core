288360bc73cd9a912e62c0b6eee95f8a856fa15a
Eugene Frolov <eugene@frolov.net.ru>
2025-03-19 10:51:29 +0300

    Switch base classes from `ModelWithNameDesc` to `ModelWithRequiredNameDesc`
    Updated `User`, `Role`, `Permission`, `Organization`, `Project`, `Idp`, and `IamClient` models to inherit from `ModelWithRequiredNameDesc`, enforcing required `name` and `description` fields across these entities.
    
    **Add `surname` and `phone` fields to the `User` model**
    - Introduced `surname` as an optional string field (max length 128) with a default empty string.
    - Added `phone` as an optional string field (max length 15) with a default value of `None`.
    - Included a new migration (`0011-add-surname-and-phone-ea974b.py`) to add corresponding columns (`surname` and `phone`) to the `iam_users` database table.
    
    **Bump `restalchemy` dependency to version 12.7.0**
    Updated `requirements.txt` to require `restalchemy>=12.7.0`, aligning with changes that depend on newer features or fixes in the library.

---
+++ migrations/0011-add-surname-and-phone-ea974b.py
@@ -0,0 +1,63 @@
+# Copyright 2016 Eugene Frolov <eugene@frolov.net.ru>
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from restalchemy.storage.sql import migrations
+
+
+class MigrationStep(migrations.AbstarctMigrationStep):
+
+    def __init__(self):
+        self._depends = ["0010-image-for-machine-31471a.py"]
+
+    @property
+    def migration_id(self):
+        return "ea974b0f-e5d0-4e17-8543-3770eca8df85"
+
+    @property
+    def is_manual(self):
+        return False
+
+    def upgrade(self, session):
+        session.execute(
+            """
+            ALTER TABLE "iam_users"
+            ADD COLUMN surname VARCHAR(128) NOT NULL DEFAULT ''
+        """
+        )
+        session.execute(
+            """
+            ALTER TABLE "iam_users"
+            ADD COLUMN phone VARCHAR(15)
+        """
+        )
+
+    def downgrade(self, session):
+        session.execute(
+            """
+            ALTER TABLE "iam_users"
+            DROP COLUMN surname
+        """
+        )
+        session.execute(
+            """
+            ALTER TABLE "iam_users"
+            DROP COLUMN phone
+        """
+        )
+
+
+migration_step = MigrationStep()
