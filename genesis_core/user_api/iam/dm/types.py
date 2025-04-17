94914e5bb14f6bd3699e4d204bb52ab889542f18
Eugene Frolov <eugene@frolov.net.ru>
2025-04-17 12:04:10 +0300

    Fix user names validation

--- genesis_core/user_api/iam/dm/types.py
+++ genesis_core/user_api/iam/dm/types.py
@@ -57,7 +57,7 @@ class Name(types.BaseCompiledRegExpType):
 
     def __init__(self, min_length=1, max_length=128):
         pattern = re.compile(
-            r"^(?![^-]*\d)(?!-)[\w`_.-]*(?<!-)$",
+            r"^(?![^-]*\d)(?!-)(?!\s)[\w`'_. -]*(?<!-)(?<!\s)$",
             flags=re.UNICODE,
         )
         super().__init__(pattern=pattern)
