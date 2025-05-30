beadd32b1a06ccc29409ae257953d1b27e039c4e
George Melikov <mail@gmelikov.ru>
2025-05-31 00:56:28 +0300

    Introduce basic DNSaaS
    Signed-off-by: George Melikov <mail@gmelikov.ru>

---
+++ genesis_core/tests/functional/service/dns/conftest.py
@@ -0,0 +1,99 @@
+#    Copyright 2025 Genesis Corporation.
+#
+#    All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import annotations
+
+import logging
+import os
+import socket
+import subprocess
+from contextlib import closing
+import typing as tp
+import uuid as sys_uuid
+from urllib.parse import urlparse
+
+import pytest
+
+from restalchemy.tests.functional import consts as ra_c
+
+
+LOG = logging.getLogger(__name__)
+
+PDNS_BIN = "/usr/sbin/pdns_server"
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+@pytest.fixture()
+def pdns_server(user_api, tmp_path_factory: pytest.TempPathFactory):
+    result = urlparse(ra_c.DATABASE_URI)
+    if result.scheme != "postgresql":
+        pytest.skip("Only PostgreSQL is supported for PowerDNS tests")
+    if not os.path.exists(PDNS_BIN):
+        pytest.skip(
+            "PowerDNS server binary not found, dataplane can't be checked"
+        )
+
+    port = result.port
+
+    directory = tmp_path_factory.mktemp("pdns")
+    config_file = directory / "pdns.conf"
+    port = find_free_port()
+
+    config = f"""\
+local-port={port}
+launch=gpgsql
+gpgsql-dbname={result.path[1:]}
+gpgsql-user={result.username}
+gpgsql-password={result.password}
+gpgsql-host={result.hostname}
+loglevel=100
+query-logging=yes
+log-dns-queries=yes
+"""
+    with open(config_file, "w") as f:
+        f.write(config)
+
+    proc = subprocess.Popen(
+        [
+            PDNS_BIN,
+            "--guardian=no",
+            "--daemon=no",
+            "--disable-syslog",
+            "--log-timestamp=no",
+            "--write-pid=no",
+            "--socket-dir=" + str(directory),
+            "--config-dir=" + str(directory),
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+    # Check it started successfully
+    assert not proc.poll(), proc.stdout.read().decode("utf-8")
+
+    yield port
+
+    proc.terminate()
+
+    # Useful to debug if things go wrong, will be shown only on test failure.
+    LOG.warning("PDNS log:")
+    LOG.warning(proc.stdout.read().decode("utf-8"))
