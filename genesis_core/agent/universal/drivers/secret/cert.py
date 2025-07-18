95a0a67d392891f2a06edb945542346d16a0f1b8
Anton Kremenetsky <anton.kremenetsky@gmail.com>
2025-07-18 16:05:43 +0300

    First implementation of certificates
    The Secret Manager service is extended with an ability to issue
    certificates. Only `dns_core` method/provider is supported for the first
    version. This method supposed DNS challenges via Core DNS that is
    available from the internet.
    
    Examples:
    
    ```bash
    curl --location 'http://10.20.0.2:11010/v1/secret/certificates/' \
    --header 'Content-Type: application/json' \
    --header 'Authorization: Bearer MY_TOKEN' \
    --data-raw '{
        "name": "my-cert",
        "project_id": "00000000-0000-0000-0000-000000000000",
        "method": {
            "kind": "dns_core"
        },
        "constructor": {
            "kind": "plain"
        },
        "email": "user@genesis-core.tech",
        "domains": ["test0.cdns.genesis-core.tech"]
    }'
    ```
    
    Also it's possible to specify domains with wildcards.
    
    ```bash
    curl --location 'http://10.20.0.2:11010/v1/secret/certificates/' \
    --header 'Content-Type: application/json' \
    --header 'Authorization: Bearer MY_TOKEN' \
    --data-raw '{
        "name": "my-cert",
        "project_id": "00000000-0000-0000-0000-000000000000",
        "method": {
            "kind": "dns_core"
        },
        "constructor": {
            "kind": "plain"
        },
        "email": "user@genesis-core.tech",
        "domains": ["*.test1.cdns.genesis-core.tech", "test1.cdns.genesis-core.tech"]
    }'
    ```

---
+++ genesis_core/agent/universal/drivers/secret/cert.py
@@ -0,0 +1,58 @@
+#    Copyright 2025 Genesis Corporation.
+#
+#    All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import annotations
+
+import logging
+
+from gcl_sdk.agents.universal.drivers import direct
+from gcl_sdk.agents.universal.storage import fs
+from gcl_sdk.clients.http import base as core_client_base
+from gcl_certbot_plugin import clients as dns_clients
+
+from genesis_core.agent.universal.drivers.secret.backend import (
+    cert as cert_back,
+)
+
+
+LOG = logging.getLogger(__name__)
+
+AGENT_WORK_DIR = "/var/lib/genesis/universal_agent/"
+
+
+class CoreDNSCertificateCapabilityDriver(direct.DirectAgentDriver):
+    """Certificate capability driver."""
+
+    def __init__(
+        self, user_api_base_url: str, username: str, password: str
+    ) -> None:
+        storage = fs.FileAgentStorage(AGENT_WORK_DIR, "cert_cap_storage.json")
+
+        auth = core_client_base.CoreIamAuthenticator(
+            base_url=user_api_base_url, username=username, password=password
+        )
+        dns_client = dns_clients.TinyDNSCoreClient(
+            base_url=user_api_base_url, auth=auth
+        )
+
+        client = cert_back.CertBotBackendClient(
+            dns_client, "admin@genesis-core.tech"
+        )
+
+        super().__init__(storage=storage, client=client)
+
+    def get_capabilities(self) -> list[str]:
+        """Returns a list of capabilities supported by the driver."""
+        return ["certificate"]
