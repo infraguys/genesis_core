9614a3349e36cc9c364f52e94bad16181470b9bf
Anton Kremenetsky <anton.kremenetsky@gmail.com>
2025-07-18 16:05:43 +0300

    First implementation of certificates
    The Secret Manager service is extended with an ability to issue
    certificates. Only `dns_core` method/provider is supported for the first
    version. This method supposed DNS challenges via Core DNS that is
    available from the internet.
    
    Examples:
    
    ```bash
    curl --location 'http://10.20.0.2:11010/v1/secret/certificates/' \
    --header 'Content-Type: application/json' \
    --header 'Authorization: Bearer MY_TOKEN' \
    --data-raw '{
        "name": "my-cert",
        "project_id": "00000000-0000-0000-0000-000000000000",
        "method": {
            "kind": "dns_core"
        },
        "constructor": {
            "kind": "plain"
        },
        "email": "user@genesis-core.tech",
        "domains": ["test0.cdns.genesis-core.tech"]
    }'
    ```
    
    Also it's possible to specify domains with wildcards.
    
    ```bash
    curl --location 'http://10.20.0.2:11010/v1/secret/certificates/' \
    --header 'Content-Type: application/json' \
    --header 'Authorization: Bearer MY_TOKEN' \
    --data-raw '{
        "name": "my-cert",
        "project_id": "00000000-0000-0000-0000-000000000000",
        "method": {
            "kind": "dns_core"
        },
        "constructor": {
            "kind": "plain"
        },
        "email": "user@genesis-core.tech",
        "domains": ["*.test1.cdns.genesis-core.tech", "test1.cdns.genesis-core.tech"]
    }'
    ```

---
+++ genesis_core/agent/universal/drivers/secret/backend/cert.py
@@ -0,0 +1,163 @@
+#    Copyright 2025 Genesis Corporation.
+#
+#    All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import annotations
+
+import logging
+import typing as tp
+
+from cryptography import x509
+from restalchemy.dm import filters as dm_filters
+from restalchemy.storage import exceptions as ra_exc
+from gcl_sdk.agents.universal.dm import models
+from gcl_sdk.agents.universal.clients.backend import base
+from gcl_sdk.agents.universal.clients.backend import exceptions
+
+from gcl_certbot_plugin import acme
+from gcl_certbot_plugin import clients as dns_clients
+from gcl_certbot_plugin.acme import acme_lib_client
+
+from genesis_core.secret.dm import models as secret_dm
+from genesis_core.agent.universal.drivers.secret.dm import models as driver_dm
+
+LOG = logging.getLogger(__name__)
+
+
+class CertBotBackendClient(base.AbstractBackendClient):
+    """Cert bot backend client."""
+
+    DEFAULT_PRIVATE_KEY_PATH = "/etc/genesis_core/certbot/privkey.pem"
+
+    def __init__(
+        self,
+        dns_client: dns_clients.TinyDNSCoreClient,
+        admin_email: str,
+        private_key_path: str = DEFAULT_PRIVATE_KEY_PATH,
+    ) -> None:
+        self._dns_client = dns_client
+        self._admin_email = admin_email
+        self._client_acme: acme_lib_client.ClientV2 | None = None
+        self._private_key = acme.get_or_create_client_private_key(
+            private_key_path
+        )
+
+    def _get_or_create_acme_client(self) -> acme_lib_client.ClientV2:
+        if self._client_acme is None:
+            self._client_acme = acme.get_acme_client(
+                self._private_key, self._admin_email
+            )
+        return self._client_acme
+
+    def get(self, resource: models.Resource) -> dict[str, tp.Any]:
+        """Get the resource value in dictionary format."""
+        try:
+            cert = driver_dm.Certificate.objects.get_one(
+                filters={
+                    "uuid": dm_filters.EQ(resource.uuid),
+                },
+            )
+        except ra_exc.RecordNotFound:
+            raise exceptions.ResourceNotFound(resource=resource)
+
+        return cert.to_resource_value()
+
+    def create(self, resource: models.Resource) -> dict[str, tp.Any]:
+        """Creates the resource. Returns the created resource."""
+        try:
+            self.get(resource)
+        except exceptions.ResourceNotFound:
+            pass
+        else:
+            raise exceptions.ResourceAlreadyExists(resource=resource)
+
+        cert = secret_dm.Certificate.from_ua_resource(resource)
+
+        # Create cert via DNS
+        pkey_pem, csr_pem, fullchain_pem = acme.create_cert(
+            self._get_or_create_acme_client(),
+            self._dns_client,
+            cert.domains,
+        )
+        cert_x509 = x509.load_pem_x509_certificate(fullchain_pem.encode())
+        expiration_at = cert_x509.not_valid_after_utc
+
+        # Build storagable password and save
+        driver_cert = driver_dm.Certificate.from_cert_resource(
+            resource, pkey_pem, csr_pem, fullchain_pem, expiration_at
+        )
+
+        driver_cert.save()
+        return driver_cert.to_resource_value()
+
+    def update(self, resource: models.Resource) -> dict[str, tp.Any]:
+        """Update the resource. Returns the updated resource."""
+        try:
+            cert = driver_dm.Certificate.objects.get_one(
+                filters={
+                    "uuid": dm_filters.EQ(resource.uuid),
+                },
+            )
+        except ra_exc.RecordNotFound:
+            raise exceptions.ResourceNotFound(resource=resource)
+
+        # TODO(akremenetsky): It's tricky logic to update the cert
+        # if domains changed. Need to check domains intersection,
+        # check wildcards
+        if set(cert["meta"]["domains"]) != set(resource.value["domains"]):
+            LOG.error("Not implemented yet")
+            raise NotImplementedError("Domains changed")
+            # return cert.to_resource_value()
+
+        # Should the cert be renewed?
+        if not cert.is_under_threshold(cert):
+            return cert.to_resource_value()
+
+        pkey_pem, csr_pem, fullchain_pem = acme.renew_cert(
+            self._get_or_create_acme_client(),
+            self._dns_client,
+            resource.value["domains"],
+            cert.pkey.encode(),
+        )
+        cert_x509 = x509.load_pem_x509_certificate(fullchain_pem.encode())
+        expiration_at = cert_x509.not_valid_after_utc
+
+        driver_cert = driver_dm.Certificate.from_cert_resource(
+            resource, pkey_pem, csr_pem, fullchain_pem, expiration_at
+        )
+
+        cert.delete()
+        driver_cert.save()
+        return driver_cert.to_resource_value()
+
+    def list(self, kind: str, **kwargs) -> list[dict[str, tp.Any]]:
+        """Lists all resources by kind."""
+        certs = driver_dm.Certificate.objects.get_all()
+
+        return [cert.to_resource_value() for cert in certs]
+
+    def delete(self, resource: models.Resource) -> None:
+        """Delete the resource."""
+        try:
+            self.get(resource)
+        except exceptions.ResourceNotFound:
+            raise exceptions.ResourceNotFound(resource=resource)
+
+        cert = driver_dm.Certificate.objects.get_one(
+            filters={
+                "uuid": dm_filters.EQ(resource.uuid),
+            }
+        )
+        acme.revoke_cert(self._get_or_create_acme_client(), cert.fullchain)
+        cert.delete()
--- genesis_core/secret/service.py
+++ genesis_core/secret/service.py
@@ -16,7 +16,6 @@
 from __future__ import annotations
 
 import logging
-import collections
 import typing as tp
 import uuid as sys_uuid
 
@@ -52,15 +51,34 @@ class SecretServiceBuilder(basic.BasicService):
     ) -> list[ua_models.TargetResource]:
         return models.Password.get_deleted_passwords(limit=limit)
 
+    def _get_new_certificates(
+        self,
+        limit: int = sc.DEFAULT_SQL_LIMIT,
+    ) -> list[models.Certificate]:
+        return models.Certificate.get_new_certificates(limit=limit)
+
+    def _get_changed_certificates(
+        self,
+        limit: int = sc.DEFAULT_SQL_LIMIT,
+    ) -> list[models.Certificate]:
+        return models.Certificate.get_updated_certificates(limit=limit)
+
+    def _get_deleted_certificates(
+        self,
+        limit: int = sc.DEFAULT_SQL_LIMIT,
+    ) -> list[ua_models.TargetResource]:
+        return models.Certificate.get_deleted_certificates(limit=limit)
+
     def _get_outdated_resources(
         self,
+        kind: str,
         limit: int = sc.DEFAULT_SQL_LIMIT,
     ) -> dict[
         sys_uuid.UUID,  # Resource UUID
         tuple[ua_models.TargetResource, ua_models.Resource],
     ]:
         outdated = ua_models.OutdatedResource.objects.get_all(
-            filters={"kind": dm_filters.EQ(sc.PASSWORD_KIND)},
+            filters={"kind": dm_filters.EQ(kind)},
             limit=limit,
         )
         return {
@@ -71,77 +89,135 @@ class SecretServiceBuilder(basic.BasicService):
             for pair in outdated
         }
 
-    def _get_outdated_passwords(
-        self, uuids: tp.Collection[sys_uuid.UUID]
-    ) -> list[models.Password]:
-        return models.Password.objects.get_all(
+    def _get_outdated_secrets(
+        self,
+        model: models.Secret,
+        uuids: tp.Collection[sys_uuid.UUID],
+    ) -> list[models.Secret]:
+        return model.objects.get_all(
             filters={"uuid": dm_filters.In(str(p) for p in uuids)},
         )
 
-    def _actualize_new_passwords(
-        self, passwords: list[models.Password] | None = None
+    def _actualize_new_secrets(
+        self, kind: str, secrets: tp.Collection[models.Secret]
     ) -> None:
-        """Actualize new passwords."""
-        passwords = passwords or self._get_new_passwords()
-
-        if len(passwords) == 0:
-            return
-
-        # Just create resources for new passwords
-        for password in passwords:
-            password_resource = password.to_ua_resource(sc.PASSWORD_KIND)
+        """Actualize new secrets."""
+        # Just create resources for new secrets
+        for secret in secrets:
+            secret_resource = secret.to_ua_resource(kind)
             try:
-                password_resource.insert()
-                password.status = sc.SecretStatus.IN_PROGRESS.value
-                password.save()
+                secret_resource.insert()
+                secret.status = sc.SecretStatus.IN_PROGRESS.value
+                secret.save()
 
-                # TODO(akremenetsky): Improve this snippet in the future
-                password_resource.tracked_at = password.updated_at
-                password_resource.status = password.status
-                password_resource.update()
+                # Commit tracked_at ts
+                secret_resource.tracked_at = secret.updated_at
+                secret_resource.status = secret.status
+
+                secret_resource.update()
                 LOG.info(
-                    "Password resource %s created", password_resource.uuid
+                    "Certificate resource %s created", secret_resource.uuid
                 )
             except Exception:
-                LOG.exception(
-                    "Error creating password resource %s", password.uuid
-                )
+                LOG.exception("Error creating cert resource %s", secret.uuid)
 
-    def _actualize_changed_passwords(self) -> None:
-        """Actualize passwords changed by user."""
-        changed_passwords = {p.uuid: p for p in self._get_changed_passwords()}
-
-        if len(changed_passwords) == 0:
+    def _actualize_changed_secrets(
+        self, kind: str, changed_secrets: dict[sys_uuid.UUID, models.Secret]
+    ) -> None:
+        """Actualize secrets changed by user."""
+        if len(changed_secrets) == 0:
             return
 
-        password_resources = ua_models.TargetResource.objects.get_all(
+        secret_resources = ua_models.TargetResource.objects.get_all(
             filters={
-                "uuid": dm_filters.In(
-                    str(p) for p in changed_passwords.keys()
-                ),
-                "kind": dm_filters.EQ(sc.PASSWORD_KIND),
+                "uuid": dm_filters.In(str(p) for p in changed_secrets.keys()),
+                "kind": dm_filters.EQ(kind),
             }
         )
 
-        # Update every resource in accordance with the new password
-        for resource in password_resources:
-            password = changed_passwords[resource.uuid]
-            new_resource = password.to_ua_resource(sc.PASSWORD_KIND)
+        # Update every resource in accordance with the new secret
+        for resource in secret_resources:
+            secret = changed_secrets[resource.uuid]
+            new_resource = secret.to_ua_resource(kind)
 
             # Update the original resource
             resource.update_value(new_resource)
             try:
-                password.status = sc.SecretStatus.IN_PROGRESS.value
-                password.save()
+                secret.status = sc.SecretStatus.IN_PROGRESS.value
+                secret.save()
 
-                resource.tracked_at = password.updated_at
-                resource.status = password.status
+                # Commit the tracked_at timestamp
+                resource.tracked_at = secret.updated_at
+                resource.status = secret.status
                 resource.update()
-                LOG.debug("Password resource %s updated", resource.uuid)
+                LOG.debug(
+                    "Secret(%s) resource %s updated", kind, resource.uuid
+                )
             except Exception:
                 LOG.exception(
-                    "Error updating password resource %s", resource.uuid
+                    "Error updating secret(%s) resource %s",
+                    kind,
+                    resource.uuid,
                 )
+
+    def _actualize_outdated_secrets(
+        self,
+        kind: str,
+        model: models.Secret,
+        secret_handler: tp.Callable[
+            [models.Secret, ua_models.TargetResource, ua_models.Resource], None
+        ],
+    ) -> None:
+        """Actualize outdated secrets.
+
+        It means some changes occurred in the system and the secrets
+        are outdated now. For instance, their status is incorrect.
+        """
+        resource_map = self._get_outdated_resources(kind)
+
+        if len(resource_map) == 0:
+            return
+
+        secrets = self._get_outdated_secrets(model, tuple(resource_map.keys()))
+
+        for secret in secrets:
+            target, actual = resource_map[secret.uuid]
+            try:
+                secret_handler(secret, target, actual)
+                LOG.debug("Secret(%s) %s actualized", kind, secret.uuid)
+            except Exception:
+                LOG.exception(
+                    "Error actualizing secret(%s) %s", kind, secret.uuid
+                )
+
+    def _actualize_deleted_secrets(
+        self, secrets: tp.Collection[ua_models.TargetResource]
+    ) -> None:
+        """Actualize secrets deleted by user."""
+        for secret in secrets:
+            try:
+                secret.delete()
+                LOG.debug(
+                    "Outdated resource(%s) %s deleted",
+                    secret.kind,
+                    secret.uuid,
+                )
+            except Exception:
+                LOG.exception(
+                    "Error deleting resource(%s) %s", secret.kind, secret.uuid
+                )
+
+    # Passwords
+
+    def _actualize_new_passwords(self) -> None:
+        """Actualize new passwords."""
+        passwords = self._get_new_passwords()
+        self._actualize_new_secrets(sc.PASSWORD_KIND, passwords)
+
+    def _actualize_changed_passwords(self) -> None:
+        """Actualize passwords changed by user."""
+        changed_passwords = {p.uuid: p for p in self._get_changed_passwords()}
+        self._actualize_changed_secrets(sc.PASSWORD_KIND, changed_passwords)
 
     def _actualize_outdated_password(
         self,
@@ -151,25 +227,36 @@ class SecretServiceBuilder(basic.BasicService):
     ) -> None:
         """Actualize outdated password."""
         password_updated = False
-        saved_password = models.Password.from_ua_resource(actual_resource)
+        status_updated = False
+        actual_pass = models.Password.from_ua_resource(actual_resource)
+
+        # `ACTIVE` only if the hash is the same
+        if (
+            actual_resource.status == sc.SecretStatus.ACTIVE
+            and target_resource.hash == actual_resource.hash
+        ):
+            status_updated = True
+        elif (
+            actual_resource.status != sc.SecretStatus.ACTIVE
+            and target_resource.status != actual_resource.status
+        ):
+            status_updated = True
 
         # Actualize password
-        if (
-            saved_password.status != password.status
-            or saved_password.value != password.value
-        ):
-            password.status = saved_password.status
-            password.value = saved_password.value
+        if status_updated or actual_pass.value != password.value:
+            if status_updated:
+                password.status = actual_pass.status
+            password.value = actual_pass.value
             password.save()
             password_updated = True
 
         # Actualize resource
         if (
             password_updated
-            or actual_resource.status != target_resource.status
             or actual_resource.full_hash != target_resource.full_hash
         ):
-            target_resource.status = actual_resource.status
+            if status_updated:
+                target_resource.status = actual_resource.status
             target_resource.full_hash = actual_resource.full_hash
             target_resource.tracked_at = password.updated_at
             target_resource.update()
@@ -177,56 +264,143 @@ class SecretServiceBuilder(basic.BasicService):
     def _actualize_outdated_passwords(self) -> None:
         """Actualize outdated passwords.
 
-        It means some changes oscurred in the system and the passwords
+        It means some changes occurred in the system and the passwords
         are outdated now. For instance, their status is incorrect.
         """
-        resource_map = self._get_outdated_resources()
-
-        if len(resource_map) == 0:
-            return
-
-        passwords = self._get_outdated_passwords(tuple(resource_map.keys()))
-
-        for password in passwords:
-            target, actual = resource_map[password.uuid]
-            try:
-                self._actualize_outdated_password(password, target, actual)
-                LOG.debug("Password %s actualized", password.uuid)
-            except Exception:
-                LOG.exception("Error actualizing password %s", password.uuid)
+        self._actualize_outdated_secrets(
+            sc.PASSWORD_KIND,
+            models.Password,
+            self._actualize_outdated_password,
+        )
 
     def _actualize_deleted_passwords(self) -> None:
         """Actualize passwords deleted by user."""
         deleted_passwords = self._get_deleted_passwords()
+        self._actualize_deleted_secrets(deleted_passwords)
 
-        if len(deleted_passwords) == 0:
-            return
+    # Certificates
 
-        for password in deleted_passwords:
-            try:
-                password.delete()
-                LOG.debug("Outdated resource %s deleted", password.uuid)
-            except Exception:
-                LOG.exception("Error deleting resource %s", password.uuid)
+    def _actualize_new_certificates(self) -> None:
+        """Actualize new certificates."""
+        certs = self._get_new_certificates()
+        self._actualize_new_secrets(sc.CERTIFICATE_KIND, certs)
+
+    def _actualize_changed_certificates(self) -> None:
+        """Actualize certificates changed by user."""
+        changed_certs = {
+            crt.uuid: crt for crt in self._get_changed_certificates()
+        }
+        self._actualize_changed_secrets(sc.CERTIFICATE_KIND, changed_certs)
+
+    def _actualize_outdated_certificate(
+        self,
+        certificate: models.Certificate,
+        target_resource: ua_models.TargetResource,
+        actual_resource: ua_models.Resource,
+    ) -> None:
+        """Actualize outdated certificate."""
+        certificate_updated = False
+        status_updated = False
+        actual_cert = models.Certificate.from_ua_resource(actual_resource)
+
+        # `ACTIVE` only if the hash is the same
+        if (
+            actual_resource.status == sc.SecretStatus.ACTIVE
+            and target_resource.hash == actual_resource.hash
+        ):
+            status_updated = True
+        elif (
+            actual_resource.status != sc.SecretStatus.ACTIVE
+            and target_resource.status != actual_resource.status
+        ):
+            status_updated = True
+
+        # Actualize certificate
+        if (
+            status_updated
+            or actual_cert.key != certificate.key
+            or actual_cert.cert != certificate.cert
+            or actual_cert.expiration_at != certificate.expiration_at
+        ):
+            if status_updated:
+                certificate.status = actual_cert.status
+            certificate.key = actual_cert.key
+            certificate.cert = actual_cert.cert
+            certificate.expiration_at = actual_cert.expiration_at
+            certificate.save()
+            certificate_updated = True
+
+        # Actualize resource
+        if (
+            certificate_updated
+            or actual_resource.full_hash != target_resource.full_hash
+        ):
+            if status_updated:
+                target_resource.status = actual_resource.status
+            target_resource.full_hash = actual_resource.full_hash
+            target_resource.tracked_at = certificate.updated_at
+            target_resource.update()
+
+    def _actualize_outdated_certificates(self) -> None:
+        """Actualize outdated certificates.
+
+        It means some changes occurred in the system and the certificates
+        are outdated now. For instance, their status is incorrect.
+        """
+        self._actualize_outdated_secrets(
+            sc.CERTIFICATE_KIND,
+            models.Certificate,
+            self._actualize_outdated_certificate,
+        )
+
+    def _actualize_deleted_certificates(self) -> None:
+        """Actualize certificates deleted by user."""
+        deleted_certificates = self._get_deleted_certificates()
+        self._actualize_deleted_secrets(deleted_certificates)
+
+    def _actualize_passwords(self) -> None:
+        try:
+            self._actualize_new_passwords()
+        except Exception:
+            LOG.exception("Error actualizing new passwords")
+
+        try:
+            self._actualize_changed_passwords()
+        except Exception:
+            LOG.exception("Error actualizing changed passwords")
+
+        try:
+            self._actualize_outdated_passwords()
+        except Exception:
+            LOG.exception("Error actualizing outdated passwords")
+
+        try:
+            self._actualize_deleted_passwords()
+        except Exception:
+            LOG.exception("Error actualizing deleted passwords")
+
+    def _actualize_certificates(self) -> None:
+        try:
+            self._actualize_new_certificates()
+        except Exception:
+            LOG.exception("Error actualizing new certificates")
+
+        try:
+            self._actualize_changed_certificates()
+        except Exception:
+            LOG.exception("Error actualizing changed certificates")
+
+        try:
+            self._actualize_outdated_certificates()
+        except Exception:
+            LOG.exception("Error actualizing outdated certificates")
+
+        try:
+            self._actualize_deleted_certificates()
+        except Exception:
+            LOG.exception("Error actualizing deleted certificates")
 
     def _iteration(self) -> None:
         with contexts.Context().session_manager():
-            try:
-                self._actualize_new_passwords()
-            except Exception:
-                LOG.exception("Error actualizing new passwords")
-
-            try:
-                self._actualize_changed_passwords()
-            except Exception:
-                LOG.exception("Error actualizing changed passwords")
-
-            try:
-                self._actualize_outdated_passwords()
-            except Exception:
-                LOG.exception("Error actualizing outdated passwords")
-
-            try:
-                self._actualize_deleted_passwords()
-            except Exception:
-                LOG.exception("Error actualizing deleted passwords")
+            self._actualize_passwords()
+            self._actualize_certificates()
